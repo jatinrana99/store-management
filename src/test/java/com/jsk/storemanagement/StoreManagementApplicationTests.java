662f67646b39e0646d0dbfaaab9bb5aa70619d37
Jatin rana <engr.jatinrana@gmail.com>
2023-02-25 23:26:51 +0530

    first commit

---
+++ src/test/java/com/jsk/storemanagement/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsk.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
