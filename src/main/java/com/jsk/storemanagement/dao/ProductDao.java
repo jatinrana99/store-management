4423b517b10be315f4132f8209ecb39afbc40455
Kushalsharmaumang <kushalsharmaumang007@gmail.com>
2023-02-27 00:09:11 +0530

    completed product Dto and Dao

--- src/main/java/com/jsk/storemanagement/dao/ProductDao.java
+++ src/main/java/com/jsk/storemanagement/dao/ProductDao.java
@@ -1,8 +1,12 @@
 package com.jsk.storemanagement.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.jsk.storemanagement.dto.Product;
 import com.jsk.storemanagement.repository.ProductRepository;
 
 @Repository
@@ -10,5 +14,44 @@ public class ProductDao {
 
     @Autowired
     ProductRepository productRepository;
-    
+
+    // find by id of product
+    public Product findById(int productId) {
+        Optional<Product> optional = productRepository.findById(productId);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        return null;
+    }
+
+    //insert product
+    public void insert(Product passProduct) {
+        productRepository.save(passProduct);
+    }
+
+    //update product
+    public void update(Product passProduct ,int productId) {
+    Product dbProduct=productRepository.findById(productId).get();
+    if(passProduct.getProductPrice()!=0.0){
+        dbProduct.setProductPrice(passProduct.getProductPrice());
+
+    }
+    if(dbProduct.getProductName()!=null){
+        dbProduct.setProductName(passProduct.getProductName());
+    }   
+    if(dbProduct.getProductAvailability()!=false){
+        dbProduct.setProductAvailability(passProduct.getProductAvailability());
+    }
+    productRepository.save(dbProduct);
+
+    }
+    // delete product
+    public void deleteProduct(int productId){
+        productRepository.deleteById(productId);
+
+    }
+    //display product
+    public List<Product> displayProduct(){
+        return productRepository.findAll();
+    }
 }
--- src/main/java/com/jsk/storemanagement/dto/Product.java
+++ src/main/java/com/jsk/storemanagement/dto/Product.java
@@ -9,35 +9,40 @@ import jakarta.persistence.ManyToOne;
 public class Product {
 
     @Id
-    int productId;
-    String productName;
-
-    @ManyToOne
-
-    Store store;
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Store getStore() {
-        return store;
-    }
-
-    public void setStore(Store store) {
-        this.store = store;
-    }
-
+   private int productId;
+   private String productName;
+   private double productPrice;
+   private boolean productAvailability;
+   @ManyToOne
+   private Store store;
+public int getProductId() {
+    return productId;
+}
+public void setProductId(int productId) {
+    this.productId = productId;
+}
+public String getProductName() {
+    return productName;
+}
+public void setProductName(String productName) {
+    this.productName = productName;
+}
+public double getProductPrice() {
+    return productPrice;
+}
+public void setProductPrice(double productPrice) {
+    this.productPrice = productPrice;
+}
+public boolean getProductAvailability() {
+    return productAvailability;
+}
+public void setProductAvailability(boolean productAvailability) {
+    this.productAvailability = productAvailability;
+}
+public Store getStore() {
+    return store;
+}
+public void setStore(Store store) {
+    this.store = store;
+}
 }
