0d30547299cbcc4df3c6d7474c3d5084bfa25795
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-28 23:55:21 +0530

    updated service of product location manager

--- src/main/java/com/jsk/storemanagement/service/ProductService.java
+++ src/main/java/com/jsk/storemanagement/service/ProductService.java
@@ -1,14 +1,41 @@
 package com.jsk.storemanagement.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jsk.storemanagement.dao.ProductDao;
+import com.jsk.storemanagement.dto.Product;
 
 @Service
 public class ProductService {
 
     @Autowired
     ProductDao productDao;
-    
+    // find by id of product
+    public Product findById(int productId) {
+        return productDao.findById(productId);
+    }
+
+    // insert product
+    public void insertProduct(Product passProduct) {
+        productDao.insertProduct(passProduct);
+    }
+
+    // update product
+    public void updateProduct(Product passProduct, int productId) {
+      productDao.updateProduct(passProduct, productId);
+
+    }
+
+    // delete product
+    public void deleteProduct(int productId) {
+        productDao.deleteProduct(productId);
+    }
+
+    // display product
+    public List<Product> displayProduct() {
+        return productDao.displayProduct();
+    }
 }
