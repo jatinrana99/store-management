281b02bf146a8edf6a36acc5d9709ce09b04b1bf
Jatin rana <engr.jatinrana@gmail.com>
2023-03-02 22:56:15 +0530

    manger and product service done

--- src/main/java/com/jsk/storemanagement/service/ManagerService.java
+++ src/main/java/com/jsk/storemanagement/service/ManagerService.java
@@ -6,35 +6,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jsk.storemanagement.dao.ManagerDao;
+import com.jsk.storemanagement.dao.StoreDao;
 import com.jsk.storemanagement.dto.Manager;
+import com.jsk.storemanagement.dto.Store;
 
 @Service
 public class ManagerService {
 
     @Autowired
     ManagerDao managerDao;
+
+    @Autowired
+    StoreDao storeDao;
+
     // find by id manager
     public Manager findById(int managerId) {
-       return managerDao.findById(managerId);
+        return managerDao.findById(managerId);
     }
 
     // insert method for manager
-    public void insertManager(Manager passManager) {
-        managerDao.insertManager(passManager);
+    public String insertManager(Manager passManager) {
+        if (findById(passManager.getManagerId()) != null) {
+            managerDao.insertManager(passManager);
+            return "Manager Inserted";
+        }
+        return "Manager with this id already exists";
     }
 
     // update method for manager
     public void updateManager(Manager passManager, int managerId) {
+        if (findById(managerId) != null) {
+            Manager dbManager = findById(managerId);
+
+            List<Store> passStores = passManager.getStore();
+            if (passManager.getStore() != null) {
+                List<Store> dbstores = dbManager.getStore();
+            }
+
+            for (Store passStore : passStores) {
+                if (storeDao.findById(passStore.getStoreId()) == null) {
+                    storeDao.insertStore(passStore);
+                }
+            }
+        }
+
         managerDao.updateManager(passManager, managerId);
     }
 
     // delete method for manager
-    public void deleteManager(int managerId) {
-        managerDao.deleteManager(managerId);
+    public String deleteManager(int managerId) {
+        if (findById(managerId) != null) {
+            managerDao.deleteManager(managerId);
+            return "Manager Deleted";
+        }
+        return "Manager with this id does not exist";
     }
 
     // display method for manager
     public List<Manager> displayManager() {
-        return managerDao.displayManager();
+        if (managerDao.displayManager() != null) {
+            return managerDao.displayManager();
+        }
+        return null;
     }
 }
--- src/main/java/com/jsk/storemanagement/service/ProductService.java
+++ src/main/java/com/jsk/storemanagement/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jsk.storemanagement.dao.CategoryDao;
 import com.jsk.storemanagement.dao.ProductDao;
 import com.jsk.storemanagement.dto.Product;
 
@@ -13,29 +14,60 @@ public class ProductService {
 
     @Autowired
     ProductDao productDao;
+
+    @Autowired
+    CategoryDao categoryDao;
+
     // find by id of product
     public Product findById(int productId) {
         return productDao.findById(productId);
     }
 
     // insert product
-    public void insertProduct(Product passProduct) {
-        productDao.insertProduct(passProduct);
+    public String insertProduct(Product passProduct) {
+        if (findById(passProduct.getProductId()) != null) {
+            productDao.insertProduct(passProduct);
+            return "product inserted";
+        }
+        return "Product with this Id already exists";
     }
 
     // update product
-    public void updateProduct(Product passProduct, int productId) {
-      productDao.updateProduct(passProduct, productId);
+    public String updateProduct(Product passProduct, int productId) {
+        if (findById(productId) != null) {
+            Product dbProduct = findById(productId);
+
+            if (passProduct.getCategory() != null) {
+                if (passProduct.getCategory().getCategoryId() != dbProduct.getCategory().getCategoryId()) {
+                    categoryDao.insertCategory(passProduct.getCategory());
+                }
+            }
+
+            productDao.updateProduct(passProduct, productId);
+            return "Product Updated";
+
+        }
+        return "Product not found with this id ";
 
     }
 
     // delete product
-    public void deleteProduct(int productId) {
-        productDao.deleteProduct(productId);
+    public String deleteProduct(int productId) {
+        if (findById(productId) != null) {
+            productDao.deleteProduct(productId);
+            return "product Id is deleted";
+        }
+        return "Product with this Id does not exist";
     }
 
     // display product
     public List<Product> displayProduct() {
-        return productDao.displayProduct();
+
+        if (productDao.displayProduct() != null) {
+            return productDao.displayProduct();
+
+        }
+        return null;
+
     }
 }
